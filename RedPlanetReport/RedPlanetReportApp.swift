3283f2b9c9a286debfd8afa96a65ee3d734ef93c
Sizwe Maluleke <sizwe.maluleke@byteorbit.com>
2023-08-05 13:56:46 +0200

    Initial Commit

---
+++ RedPlanetReport/RedPlanetReportApp.swift
@@ -0,0 +1,17 @@
+//
+//  RedPlanetReportApp.swift
+//  RedPlanetReport
+//
+//  Created by Sizwe Maluleke on 2023/08/05.
+//
+
+import SwiftUI
+
+@main
+struct RedPlanetReportApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
